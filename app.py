c266cf5c2b04f978efb2a1711a1e1f8f154054a3
yunusozturk@msn.com <yunusozturk@msn.com>
2023-02-03 20:54:47 +0300

    .

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import shutil
+app = Flask(__name__)
+
+def createfile():
+     shutil.copyfile("/app/temp/66.jpg", "/app/temp/68886.jpg")
+
+@app.route("/")
+def home():
+    createfile()
+    return jsonify({ 'name' : 'yunus'}) 
+
+@app.route("/CropFile")
+def CropFile():
+    createfile()
+    return jsonify({ 'name' : 'yunus'})     
+
+if (__name__ == '__main__'):
+    app.run(debug=False,port=8000, host="0.0.0.0")
